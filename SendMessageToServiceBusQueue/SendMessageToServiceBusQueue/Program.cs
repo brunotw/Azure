a38d5b279bcf90b528225c1d607705db3674b879
brunotw <bwillian@codec.ie>
2021-01-22 12:16:03 +0000

    Send Message to Service Bus Queue

---
+++ SendMessageToServiceBusQueue/SendMessageToServiceBusQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendMessageToServiceBusQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
